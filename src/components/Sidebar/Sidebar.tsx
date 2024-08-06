e5e8fa1b98ab45580e3be6bdf47d8a03840e8f80
laraxlara <laraxlara@gmail.com>
2024-08-06 14:18:42 +0200

    Update closing for sidebar on small screens

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -26,7 +26,9 @@ function Sidebar({}: Props) {
 
       <aside
         id="default-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full  sm:translate-x-0 bg-white sm:bg-gray-200"
+        className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
+          isOpen ? "" : "-translate-x-full"
+        }  sm:translate-x-0 bg-white sm:bg-gray-200`}
         aria-label="Sidebar"
       >
         <div className="relative h-full overflow-y-auto bg-white rounded-r-3xl shadow-xl overflow-auto no-scrollbar">
@@ -37,6 +39,15 @@ function Sidebar({}: Props) {
         </div>
         <SidebarFooter />
       </aside>
+
+      {isOpen ? (
+        <div
+          onClick={toggleSidebar}
+          className="w-full h-full bg-black opacity-50 absolute top-0 z-10"
+        ></div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
